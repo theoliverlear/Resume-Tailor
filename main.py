9b2e9d6fdfcda01e1cb22579635232d42827cfee
theoliverlear <sigwartholiver2@gmail.com>
2025-03-07 09:44:53 -0600

    Refactor main.py to implement dynamic skill injection and integration with `call_open_ai`

--- main.py
+++ main.py
@@ -1,30 +1,57 @@
-import os
-
 from docx import Document
-from dotenv import load_dotenv
 
-load_dotenv()
-open_ai_key = os.getenv('OPEN_AI_KEY')
+from src.ai.doc_ai import call_open_ai
 
-# document = Document("Test_Resume.docx")
-#
-# def get_editable_skills():
-#     for table in document.tables:
-#         for row in table.rows:
-#             for index, cell in enumerate(row.cells):
-#                 if "Skills" in cell.text:
-#                     index_of_skills = cell.text.index("Skills")
-#                     relevant_text = cell.text.split("Skills")[1]
-#                     skills = relevant_text.split('\n')
-#                     return skills[1:]
-#
-# def get_skill_cell():
-#     for table in document.tables:
-#         for row in table.rows:
-#             for index, cell in enumerate(row.cells):
-#                 if "Skills" in cell.text:
-#                     return cell
-#
-# skills = get_editable_skills()
-# skills = list(skills)
-# print(skills)
+document = Document("Test_Resume.docx")
+
+def get_editable_skills():
+    for table in document.tables:
+        for row in table.rows:
+            for index, cell in enumerate(row.cells):
+                if "Skills" in cell.text:
+                    relevant_text = cell.text.split("Skills")[1]
+                    skills = relevant_text.split('\n')
+                    return list(skills[1:])
+
+default_skills = get_editable_skills()
+
+def get_editable_skills_cells():
+    for table in document.tables:
+        for row in table.rows:
+            for index, cell in enumerate(row.cells):
+                if "Skills" in cell.text:
+                    return cell
+
+def get_skill_cell():
+    for table in document.tables:
+        for row in table.rows:
+            for index, cell in enumerate(row.cells):
+                if "Skills" in cell.text:
+                    return cell
+
+def inject_skills(cell, new_skills):
+    for index, skill in enumerate(default_skills):
+        if len(new_skills) > index:
+            replace_text_preserving_format(cell,
+                                           default_skills[index],
+                                           new_skills[index])
+
+def replace_text_preserving_format(cell, old_text, new_text):
+    found_skills: bool = False
+    for paragraph in cell.paragraphs:
+        for run in paragraph.runs:
+            skills_detected: bool = not found_skills and "Skills" in run.text
+            if skills_detected:
+                found_skills = True
+                continue
+            if old_text in run.text:
+                run.text = run.text.replace(old_text, new_text)
+
+
+def main():
+    cell = get_editable_skills_cells()
+    inject_skills(cell, call_open_ai())
+    document.save("Updated_Resume.docx")
+
+if __name__ == "__main__":
+    main()
